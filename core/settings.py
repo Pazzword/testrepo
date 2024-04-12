6c8f1dc211c16524a9cf63a3a5afaf89c84c85e0
Pazzword <anzor.pozner@gmail.com>
2024-04-12 21:10:06 +0100

    OPS

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "core/static")]
+STATIC_ROOT = '/home/adtest231/adtest231.pythonanywhere.com/ModalityDark/core/static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
