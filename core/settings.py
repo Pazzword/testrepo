51e33a926da9b812b1772c3d9c4909ffc7ae2568
Pazzword <anzor.pozner@gmail.com>
2024-04-13 04:02:09 +0100

    done

--- core/settings.py
+++ core/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/adtest231/adtest231.pythonanywhere.com/core/static'
+#STATIC_ROOT = '/home/adtest231/adtest231.pythonanywhere.com/core/static'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
