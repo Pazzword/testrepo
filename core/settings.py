85de5b15b00396b09fee65de48c390d3e5134e95
Pazzword <anzor.pozner@gmail.com>
2024-04-13 04:03:57 +0100

    ed

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-#STATIC_ROOT = '/home/adtest231/adtest231.pythonanywhere.com/core/static'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
@@ -128,6 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'core/static'),
 ]
+
+STATIC_ROOT = '/path/to/staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
